2e14c8270d70377867cacb4a1dba5fe83ab1b71e
alexanderbeletsky <alexander.beletsky@gmail.com>
2014-11-17 21:23:10 +0100

    dummy code

---
+++ index.js
@@ -0,0 +1,13 @@
+function ClientWrapper() {
+
+}
+
+function client(options) {
+	var wrapper = new ClientWrapper();
+
+	return {
+		createClient: function (connection) {
+			return wrapper.create(connection);
+		}
+	};
+}
